7bef2d54be9c3bb66af69245bbc2a0933e59ddd4
pranay <pranayharishchandra@gmail.com>
2024-02-12 13:24:26 +0530

    Project Section - image added

--- components/project.tsx
+++ components/project.tsx
@@ -48,7 +48,19 @@ export default function Project({
           </ul>
         </div>
 
+        <Image
+          src={imageUrl}
+          alt="Project I worked on"
+          quality={95}
+          className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
+        transition 
+        group-hover:scale-[1.04]
+        group-hover:-translate-x-3
+        group-hover:translate-y-3
+        group-hover:-rotate-2
 
+"
+        />
       </section>
     </motion.div>
   );
