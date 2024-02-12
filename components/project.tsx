edd18ff0a23d8f2dc9e5dc7666a4066ed068e4cf
pranay <pranayharishchandra@gmail.com>
2024-02-12 13:23:48 +0530

    Project Section - tags

--- components/project.tsx
+++ components/project.tsx
@@ -36,7 +36,16 @@ export default function Project({
           <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
             {description}
           </p>
-          
+          <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
+            {tags.map((tag, index) => (
+              <li
+                className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
+                key={index}
+              >
+                {tag}
+              </li>
+            ))}
+          </ul>
         </div>
 
 
