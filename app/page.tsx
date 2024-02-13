dc31f13d495793e1384987c10390a2c953a16b6d
pranay <pranayharishchandra@gmail.com>
2024-02-13 19:10:06 +0530

    Footer Done

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Projects from '@/components/projects'
 import Skills from '@/components/skills'
 import Experience from '@/components/experience'
 import Contact from '@/components/contact'
+import Footer from '@/components/footer'
 
 
 export default function Home() {
@@ -17,6 +18,7 @@ export default function Home() {
       <Skills />
       <Experience />
       <Contact />
+      <Footer />
     </main>
   )
 }
---
+++ components/footer.tsx
@@ -0,0 +1,20 @@
+
+
+import React from "react";
+
+export default function Footer() {
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className="mb-10 px-4 text-center text-gray-500 text-xs">
+      <small className="mb-2 block">
+        &copy; {currentYear} Pranay. All rights reserved.
+      </small>
+      <p className="">
+        <span className="font-semibold">About this website:</span> built with
+        React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS,
+        Framer Motion, React Email & Resend, Vercel hosting.
+      </p>
+    </footer>
+  );
+}
