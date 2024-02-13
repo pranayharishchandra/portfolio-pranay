4d4e62eb06421aab8c50b9918284e085605a77b7
pranay <pranayharishchandra@gmail.com>
2024-02-13 12:34:32 +0530

    experience section done, bug fixed by lineColor="" line 22 in experience.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import About from '@/components/about'
 import SectionDivider from '@/components/section-divider'
 import Projects from '@/components/projects'
 import Skills from '@/components/skills'
+import Experience from '@/components/experience'
 
 
 export default function Home() {
@@ -13,6 +14,7 @@ export default function Home() {
       <About />
       <Projects />
       <Skills />
+      <Experience />
     </main>
   )
 }
